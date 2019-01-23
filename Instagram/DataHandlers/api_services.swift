be805056171b341d946a65306c2e861faf0c7652
Collins <collinskariuki@gmail.com>
2019-01-23 14:08:27 +0300

    first commit

---
+++ Instagram/DataHandlers/api_services.swift
@@ -0,0 +1,30 @@
+//
+//  api_services.swift
+//  Instagram
+//
+//  Created by Collins on 1/21/19.
+//  Copyright © 2019 Collins. All rights reserved.
+//
+
+import UIKit
+
+class api_services: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram/DataHandlers/end_points.swift
@@ -0,0 +1,9 @@
+//
+//  end_points.swift
+//  Instagram
+//
+//  Created by Collins on 1/21/19.
+//  Copyright © 2019 Collins. All rights reserved.
+//
+
+import Foundation
